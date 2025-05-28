436d15820c7293753c1e03b486415376931e6d67
Tavan Thiry <tavanthiry@Arilds-Mac-mini.local>
2025-05-28 13:52:38 +0200

    added globalstyles and changed the page heading

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: "Quicksand", sans-serif;
+    background: #FFFFFF;
+    color: #1E1E1E;
+  }
+`;
