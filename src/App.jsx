10925d4c72aec7ccafef44c6888fa5a85c5e8c00
Tavan Thiry <tavanthiry@Arilds-Mac-mini.local>
2025-05-29 15:38:37 +0200

    removed lazy loading to taskform and tasklist

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
-import React, { Suspense, lazy } from "react"
 import { GlobalStyle } from "./styles/GlobalStyle"
 import { Header } from "./components/Header"
-
-// Lazy load components
-const TaskForm = lazy(() => import("./components/TaskForm"))
-const TaskList = lazy(() => import("./components/TaskList"))
+import { TaskForm } from "./components/TaskForm"
+import { TaskList } from "./components/TaskList"
 
 export const App = () => {
   return (
     <>
       <GlobalStyle />
       <Header />
-      <Suspense fallback={<div>Loading...</div>}>
       <TaskForm />
       <TaskList />
-      </Suspense>
     </>
   )
 }
